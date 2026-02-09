a3f9bdf7add9759bd439752d54417298bda694cc
Owen Holm <owenholm87@gmail.com>
2026-02-09 15:05:43 -0500

    Create main.py to initialize all components and run

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from datetime import datetime, timedelta
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from src import FinanceTracker, BalancePredictor, FinanceVisualizer, FinanceUI
+
+def main():
+    tracker = FinanceTracker()
+
+    predictor = BalancePredictor(tracker)
+
+    visualizer = FinanceVisualizer(tracker, predictor)
+
+    ui = FinanceUI(tracker, predictor, visualizer)
+
+    ui.run()
+
+    if __name__ == "__main__":
+        main()
